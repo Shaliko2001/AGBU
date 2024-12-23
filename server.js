a9e0670a57ebf3b36c2152532e25bdead58cdfee
Ilyyan <plusiphone211@gmail.com>
2024-12-23 18:01:27 +0400

    Added server.js

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const cors = require('cors');//cros origin resource shairing
+const morgan = require("morgan");
+const {createLogger,format} = require("winston"); //poxarinum e console-in
 //const User = require('./mongo.schema');
 const app = express();
 app.use(express.json());
 const api = require('./router');
 //application program interface
+const corsOptions = {
+   origin: "http://localhost:3000",  //frontendi api adreess
+   methods: "GET,POST,DELETE",// կիրառվող մեթոդներ
+   allowHeaders:["Content-Type", "Authorisation"] //info-ի ձևաչափը
+
+};
+const logger = createLogger({
+   level: 'info', 
+   format: format.combine(
+     format.timestamp(),
+     format.json()
+   )
+ });
+
+api.use(cors(corsOptions));
+api.use(morgan("dev")) //request-i maasin infon terminalum tesnelu hamar
 app.use('/api/v1', api);
+app.get("/", (req,res)=>{
+res.send("hello world")
+})
 
 require('dotenv').config();
 const port = process.env.PORT;
 
 
 app.listen(port, () => {
-   console.log('server listening 4000 port');
+logger.info("server running on port 3005")
 });
